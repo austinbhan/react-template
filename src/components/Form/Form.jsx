edba3dec9d08d630c8ea37bf63a7ed8482144737
austinbhan <austinbhan@outlook.com>
2022-09-30 14:22:45 -0700

    Form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,6 +1,10 @@
 // Import the Form Components Here
 import {
-  InputControl
+  InputControl,
+  FormButton,
+  SelectControl,
+  TextAreaControl,
+  CheckboxControl
 } from '../FormComponents/FormComponents';
 
 export default function Form() {
@@ -12,6 +16,35 @@ export default function Form() {
           name="name"
           placeholder="your name"
         />
+
+        <InputControl
+          label="Date"
+          required
+          type="date"
+          name="date"
+          placeholder="pick a date"
+        />
+
+        <SelectControl
+          label="Letter"
+          required
+          name="letter"
+          placeholder="select a letter"
+        >
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </SelectControl>
+
+        <TextAreaControl
+          label="Bio"
+          name="bio"
+          placeholder="tell me about yourself"
+        />
+
+        <CheckboxControl legend="do you accept?" label="Yes" />
+
+        <FormButton text="Submit"/>
       </form>
     </>
   );

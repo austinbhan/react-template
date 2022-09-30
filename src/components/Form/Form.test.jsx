79eb02380945d25e3bb5750b061af794c613cd2b
austinbhan <austinbhan@outlook.com>
2022-09-30 15:47:26 -0700

    Tests

--- src/components/Form/Form.test.jsx
+++ src/components/Form/Form.test.jsx
@@ -1,10 +1,9 @@
 import {  render, screen } from '@testing-library/react';
 import {
-//   CheckboxControl,
-//   FormButton,
+  CheckboxControl,
+  FormButton,
   InputControl,
   SelectControl,
-//   TextAreaControl,
 } from '../FormComponents/FormComponents.jsx';
 
 test ('Input Control', async () => {
@@ -23,14 +22,41 @@ test ('Input Control', async () => {
   expect(inputControl.required).toBe(true);
 });
 
-// test('Select Control', async () => {
-//   render(
-//     <SelectControl label="Animal" name="animal" required>
-//       <option>Cat</option>
-//       <option>Dog</option>
-//       <option>Bird</option>
-//     </SelectControl>
-//   );
+test('Select Control', async () => {
+  render(
+    <SelectControl label="Animal" name="animal" required>
+      <option>Cat</option>
+      <option>Dog</option>
+      <option>Bird</option>
+    </SelectControl>
+  );
 
-    
-// });
+  const selectControl = screen.getByLabelText('Animal');
+  expect(selectControl.options.length).toBe(3);
+  const placeHolderOption = selectControl.options[0];
+  expect(placeHolderOption.textContent).toBe('Cat');
+  expect(placeHolderOption.disabled).toBe(false);    
+});
+
+test('Checkbox Control', async () => {
+  render(
+    <CheckboxControl
+      legend="Do you accept?"
+      label="Yes"
+      name="accept"
+      required
+    />
+  );
+
+  const legend = screen.getByText('Do you accept?');
+  expect(legend).not.toBeNull();
+  const checkboxControl = screen.getByLabelText('Yes');
+  expect(checkboxControl.required).toBe(true);
+});
+
+test('Submit Button', async () => {
+  render(<FormButton text="Submit Me" />);
+
+  const button = screen.getByRole('button');
+  expect(button.textContent).toBe('');
+});
